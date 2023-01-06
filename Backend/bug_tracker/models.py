2be63f88e806a1a33d8df555c1e90abd6b1beb87
Mahletk1 <mah.kumelachew@gmail.com>
2023-01-06 20:48:49 +0300

    create users on firebase

--- Backend/bug_tracker/models.py
+++ Backend/bug_tracker/models.py
@@ -6,3 +6,4 @@ class User(models.Model):
     name = models.CharField(max_length=200)
     profile_image= models.ImageField(upload_to=upload_to, blank=True, null=True)
     role = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
--- Backend/bug_tracker/views.py
+++ Backend/bug_tracker/views.py
@@ -4,6 +4,11 @@ from .serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from firebase_admin import auth,credentials
+import firebase_admin
+
+cred = credentials.Certificate("C:/Users/mahku/Bug-Tracker/Backend/privateKey.json")
+default_app = firebase_admin.initialize_app(cred)
 
 @api_view(['GET','POST'])
 def get_users(request):
@@ -20,4 +25,14 @@ def get_users(request):
         else:
             print(serializer.errors)
             return Response(status=status.HTTP_400_BAD_REQUEST)
-       
+
+@api_view(['GET','POST'])
+def create_users(request):
+    if request.method == 'POST':
+        user = auth.create_user(
+        email=request.data['email'],
+        email_verified=False,
+        password=request.data['password'],
+        disabled=False)
+        return Response("Successfully created new user", status=status.HTTP_201_CREATED)
+        # print('Sucessfully created new user: {0}'.format(user.uid))
